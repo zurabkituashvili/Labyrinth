0183fe65676fc6154d7e62efbb9284a8ea38be76
Helmut Seidl <helmut.seidl@tum.de>
2020-10-18 21:50:54 +0200

    Exercise-Template pushed by Artemis

---
+++ src/fop/w4lab/BubbleSort.java
@@ -0,0 +1,11 @@
+package fop.w4lab;
+
+import java.util.*;
+
+public class BubbleSort {
+
+    public void performSort(List<Date> input) {
+
+        //TODO: implement
+    }
+}
---
+++ src/fop/w4lab/MergeSort.java
@@ -0,0 +1,12 @@
+package fop.w4lab;
+
+import java.util.*;
+
+public class MergeSort {
+
+    public void performSort(List<Date> input) {
+
+        //TODO: implement
+    }
+
+}
