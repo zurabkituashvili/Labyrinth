9330dc1182ff4ccf8b7ebe24beca90327713c7ef
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-04 23:49:50 +0400

    done

--- src/fop/w4lab/FindPingu.java
+++ src/fop/w4lab/FindPingu.java
@@ -17,10 +17,49 @@ public class FindPingu extends Maze {
         // maze = generatePenguinMaze(width, height);
         int penguins = walk(1, 0, maxDistance);
         System.out.println("Rescued pinguins: " + penguins);
+        draw(maze);
     }
 
     public static int walk(int x, int y, int maxDistance) {
-	// TODO
-    }
+        //constraints
+        if (maxDistance < 0)
+            return 0;
+        if (x < 0 || x >= maze.length || y < 0 || y >= maze[0].length)
+            return 0;
+        if (maze[x][y] == WALL || maze[x][y] == OLD_PATH_ACTIVE)
+            return 0;
 
+
+        int penguins = 0;
+        if (maze[x][y] == PENGUIN)
+            penguins = 1;
+
+        maze[x][y] = PLAYER;
+        draw(maze);
+        maze[x][y] = OLD_PATH_ACTIVE;
+
+        penguins += walk(x + 1, y, maxDistance - 1);
+        maze[x][y] = PLAYER;
+        draw(maze);
+        maze[x][y] = OLD_PATH_ACTIVE;
+
+
+        penguins += walk(x - 1, y, maxDistance - 1);
+        maze[x][y] = PLAYER;
+        draw(maze);
+        maze[x][y] = OLD_PATH_ACTIVE;
+
+
+        penguins += walk(x, y + 1, maxDistance - 1);
+        maze[x][y] = PLAYER;
+        draw(maze);
+        maze[x][y] = OLD_PATH_ACTIVE;
+
+        penguins += walk(x, y - 1, maxDistance - 1);
+        maze[x][y] = PLAYER;
+        draw(maze);
+        maze[x][y] = OLD_PATH_DONE;
+
+        return penguins;
+    }
 }
